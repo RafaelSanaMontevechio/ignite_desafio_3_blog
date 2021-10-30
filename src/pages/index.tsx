605f0d030cc80226a7d7b9a58152116c6c0e8dfe
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-10-30 09:33:34 -0300

    feat: :sparkles: post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
+import { FiUser } from 'react-icons/fi';
+import { FiCalendar } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import LoadMoreButton from '../components/LoadMoreButton';
 
 import commonStyles from '../styles/common.module.scss';
+
 import styles from './home.module.scss';
 
 interface Post {
@@ -30,13 +34,44 @@ interface HomeProps {
 
 export default function Home() {
   return (
-    <div className={styles.contentContainer}>
-      <LoadMoreButton
+    <>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <Link href="/posts/slug">
+            <a>
+              <strong>Como utilizar hooks</strong>
+              <p>
+                Nesse arquivo você deve configurar a importação da fonte Inter
+                do Google Fonts.
+              </p>
+              <div>
+                <span>
+                  <FiCalendar /> 19 Abr 2020
+                </span>
+                <span>
+                  <FiUser /> Rafael
+                </span>
+              </div>
+            </a>
+          </Link>
+          {/* {posts.map(post => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))} */}
+        </div>
+      </main>
+
+      {/* <LoadMoreButton
         loadMore={() => {
           console.log('more posts');
         }}
-      />
-    </div>
+      /> */}
+    </>
   );
 }
 
